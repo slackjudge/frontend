79c1978dd7a3b785e4c5f49f8b8eb24b39fd875b
tkv00 <tkv0098@gmail.com>
2025-12-02 17:08:25 +0900

    init: setup common color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,34 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
-};
+  theme: {
+    extend: {
+      colors: {
+        white: "#FFFFFF",
+        gray:{
+          100:"#EDEFF1",
+          200:"#8E8989",
+          300:"#8E8E93",
+          400:"#787880",
+        },
+        black:{
+          100:"#000000",
+          200:"#161616",
+
+        },
+        yellow:{
+          100:"#FFCE5133",
+          500:"#FFCE51"
+        },
+        orange:{
+          500:"#FF7324"
+        },
+        red:"#FF383C",
+        blue:{
+          500:"#0088FF",
+          700:"#45539D"
+        }
+      },
+    },
+  },
+  plugins: [],
+}
