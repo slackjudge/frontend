f6292a080a280b8a5c0964b0dada27784b46dfb6
andrew <leegd120@gmail.com>
2025-12-08 16:07:48 +0900

    feat/#9: LoginPage 레이아웃 및 반응형 마크업 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import { Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
 import ErrorPage from './pages/ErrorPage';
 import SignUpPage from './pages/auth/SignUpPage';
 import RankingPage from './pages/RankingPage';
 import NotificationPage from './pages/NotificationPage';
 import MyPage from './pages/MyPage';
 import Overlay from './components/common/Overlay';
+import LoginPage from './pages/auth/LoginPage';
 
 function App() {
 	return (
 		<Overlay>
 			<Routes>
-				<Route path="/" element={<HomePage />} />
+				<Route path="/" element={<LoginPage />} />
 				<Route path="/not-found" element={<ErrorPage />} />
 				<Route path="/sign-up/" element={<SignUpPage />} />
 				<Route path="/main/ranking" element={<RankingPage />} />
--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,3 +1,29 @@
-export default function Login(){
-    return <text> 로그인 페이지</text>
+import SlackLoginButton from "../../components/signup/SlackLoginButton";
+import mainLogo from "../../assets/images/main_logo.png";
+
+export default function LoginPage() {
+  const handleSlackLogin = () => {
+    window.location.href =
+      "https://slack.com/openid/connect/authorize?scope=email openid profile&client_id=10009236679351.10028267462772&redirect_uri=https://api.slackjudge.store/oauth/slack/callback&response_type=code";
+  };
+
+  return (
+    <div className="
+      min-h-screen flex flex-col items-center justify-center
+      bg-white px-4
+    ">
+      {/* 로고 */}
+      <img
+        src={mainLogo}
+        alt="Slack Judge"
+        className="
+          w-[240px] md:w-[280px] lg:w-[300px]
+          mb-14 md:mb-16
+        "
+      />
+
+      {/* 로그인 버튼 */}
+      <SlackLoginButton onClick={handleSlackLogin} />
+    </div>
+  );
 }
