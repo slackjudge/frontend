985dae950020c7b95a071cec20a9db68a027671e
rettooo <gkdud9236@naver.com>
2025-12-09 17:31:18 +0900

    feat/#17: mypage 타입 정의( api 응답구조)

---
+++ src/types/mypage.ts
@@ -0,0 +1,39 @@
+
+export interface Problem {
+    title: string;
+    tierLevel: number;
+    link: string;
+  }
+  
+  export interface SelectedDateDetail {
+    date: string;
+    dailyScore: number;
+    dailyRank: number; //일간 순위 ( 우측 패널용)
+    solvedCount: number;
+    maxTierLevel: number;
+    problems: Problem[];
+  }
+  
+  export interface Grass {
+    date: string;
+    solvedCount: number;
+    degree: number;
+  }
+  
+  export interface Profile {
+    username: string;
+    baekjoonId: string;
+    tierLevel: number; //0 ~30으로 이미지 띄움 
+    totalScore: number;
+    myRank: number;
+  }
+  
+  export interface MyPageResponse {
+    isSuccess: boolean;
+    message: string;
+    response: {
+      profile: Profile;
+      grass: Grass[];
+      selectedDateDetail: SelectedDateDetail;
+    };
+  }
