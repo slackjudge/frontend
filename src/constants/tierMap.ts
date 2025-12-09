9a0cbf280eb26d00825fd252d781049b7e48fa56
rettooo <gkdud9236@naver.com>
2025-12-09 16:32:13 +0900

    fix: fix icons change png to svg

--- src/constants/tierMap.ts
+++ src/constants/tierMap.ts
@@ -5,7 +5,7 @@ export const tierMap = (() => {
   groups.forEach((group) => {
     for (let i = 1; i <= 5; i++) {
       const prefix = group[0]; // b, s, g, p, d, r
-      map[`${group}_${i}`] = `/src/assets/icons/baekjoon/${group}/${prefix}${i}.png`;
+      map[`${group}_${i}`] = `/src/assets/icons/baekjoon/${group}/${prefix}${i}.svg`;
     }
   });
 
