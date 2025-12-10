488330922999f2e983f656e59106c755ca655451
andrew <leegd120@gmail.com>
2025-12-10 14:46:47 +0900

    fix/#18: div button으로 수정

--- src/components/signup/TeamDropDown.tsx
+++ src/components/signup/TeamDropDown.tsx
@@ -13,7 +13,8 @@ export default function TeamDropdown({ teamName, setTeamName }: TeamDropdownProp
     <div className="relative w-full">
 
       {/* 선택박스 */}
-      <div
+      <button
+        type="button"
         onClick={() => setOpen(!open)}
         className="w-full h-[45px] border border-[#DADCE0] rounded-[8px]
         px-3 flex items-center justify-between cursor-pointer bg-white"
@@ -24,22 +25,23 @@ export default function TeamDropdown({ teamName, setTeamName }: TeamDropdownProp
             : "반을 선택해주세요."}
         </span>
         <span className="text-[#C4C8CC] text-[12px]">▾</span>
-      </div>
+      </button>
 
       {/* 아래로 펼쳐지는 메뉴 */}
       {open && (
         <div className="absolute left-0 right-0 mt-1 border border-[#DADCE0] rounded-[8px] bg-white shadow z-10">
           {TEAM_OPTIONS.map((team) => (
-            <div
+            <button
               key={team.value}
+              type="button"
               onClick={() => {
                 setTeamName(team.value);
                 setOpen(false);
               }}
-              className="px-3 py-3 text-[14px] text-[#686F75] hover:bg-gray-100 cursor-pointer"
+              className="w-full block text-left px-3 py-3 text-[14px] text-[#686F75] hover:bg-gray-100 cursor-pointer"
             >
               {team.label}
-            </div>
+            </button>
           ))}
         </div>
       )}
