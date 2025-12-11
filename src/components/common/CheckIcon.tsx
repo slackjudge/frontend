17bc82132c17c17573c664ee49334f75cd4d51a6
andrew <leegd120@gmail.com>
2025-12-11 13:38:50 +0900

    feat/#26 체크표시 아이콘 추가

---
+++ src/components/common/CheckIcon.tsx
@@ -0,0 +1,14 @@
+export default function CheckIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="w-[24px] h-[24px] text-green-600"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="3"
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
+    </svg>
+  );
+}
