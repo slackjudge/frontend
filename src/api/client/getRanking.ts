156736667a6edf37f90ae1c92fdb9b16320677b5
marulog <gr214777@naver.com>
2025-12-16 16:05:28 +0900

    feat/#46 : 경로 수정

--- src/api/client/getRanking.ts
+++ src/api/client/getRanking.ts
@@ -44,7 +44,7 @@ export async function getRanking(params: {
   if (params.dateTime) qs.set("dateTime", toLocalDateTimeString(params.dateTime));
   if (params.group !== "ALL") qs.set("group", params.group);
 
-  const body = await apiFetch<RankingPage>(`/api/rank?${qs.toString()}`, { method: "GET" });
+  const body = await apiFetch<RankingPage>(`/rank?${qs.toString()}`, { method: "GET" });
 
   // apiFetch 내부에서 success=false면 이미 ApiError를 throw하도록 되어 있음
  if (!body.data) throw new Error("EMPTY_RANKING_DATA");
